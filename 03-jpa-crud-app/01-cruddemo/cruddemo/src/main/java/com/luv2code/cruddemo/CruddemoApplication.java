3039ea14eb9e3fd54ed80cce9f5946e5d242337b
Sodja Nathan  O. <sodjanathan@gmail.com>
2026-09-16 11:04:48 +0100

    exploring springboot: dao, jpa, beans etc

--- 03-jpa-crud-app/01-cruddemo/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-jpa-crud-app/01-cruddemo/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +16,47 @@ public class CruddemoApplication {
 
 		}
 	@Bean
-	public CommandLineRunner commandLineRunner(String [] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//create the student object
+		System.out.println("creating the new student...");
+		Student tempStudent = new Student("Paul", "Doe", "nathan@terrasure.com");
+
+		//save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		//display the student
+		System.out.println("Saved student. Generated id: "+ tempStudent.getId());
 	}
+
+
+	public void readStudent(StudentDAO studentDAO) {
+		// create student object
+		System.out.println("creating a student obj...");
+		Student tempStudent = new Student("nathan", "sodja", "sodjanathan@gmail.com");
+
+		//save the student
+		System.out.println("saving a student obj...");
+		studentDAO.save(tempStudent);
+
+		//display id of student
+		int theId = tempStudent.getId();
+		System.out.println("saved student. generated id: " + theId);
+
+		//retrieve student by id
+		System.out.println("reading student obj..." + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		//display student
+	}
+}
 
 
